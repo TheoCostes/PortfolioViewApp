a47d2f6864bca9a7bdb798576576cec0b5120675
TheoCostes <theo.costes@ynsect.com>
2023-07-24 09:10:31 +0200

    ajout choix preference de la currencie de base

--- suivi_portfolio/userpreferences/urls.py
+++ suivi_portfolio/userpreferences/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('transactions.urls'),),
-    path('authentication/', include('authentication.urls')),
-    path('preferences/', include('userpreferences.urls'))
-]
+    path("", views.index, name='preferences'),
+    ]
