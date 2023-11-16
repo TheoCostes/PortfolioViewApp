40cd0a6fadd8ed22279be6a7233a5303a35580a9
TheoCostes <theo.costes@ynsect.com>
2023-11-16 10:27:17 +0100

    ajout de l'initialisation des valeurs dans le formulaire de modification d'une transaction

--- pages/02_transaction.py
+++ pages/02_transaction.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
@@ -19,10 +21,6 @@ def paginate_dataframe(dataframe, page_size, page_num):
     return dataframe[offset : offset + page_size]
 
 
-def filter_token_df(df):
-    pass
-
-
 conn = sqlite3.connect("./data/db.sqlite3")
 cursor = conn.cursor()
 
@@ -40,9 +38,9 @@ col_name.remove("id")
 
 page_size = st.selectbox("nombre de ligne par page", [10 + i * 5 for i in range(10)])
 total_num = len(transaction_df) // page_size
-page_num = st.select_slider("", ["page " + str(i + 1) for i in range(total_num)])
+page_num = st.selectbox("", [i + 1 for i in range(total_num)], format_func=lambda x: "page " + str(x))
 transaction_df_page = paginate_dataframe(
-    transaction_df, page_size, int(page_num.split(" ")[-1])
+    transaction_df, page_size, page_num
 )
 
 cols = st.columns(len(col_name) + 1)
@@ -72,16 +70,23 @@ for idx, row in transaction_df_page.iterrows():
             all_token = cursor.execute("select * from transactions_token").fetchall()
             col_name_token = [description[0] for description in cursor.description]
             all_token_df = pd.DataFrame(all_token, columns=col_name_token)
+            type_actif_list = all_token_df["type"].unique().tolist()
+            symbole_list = all_token_df["symbole"].unique().tolist()
 
-            st.session_state["dict_edit_transaction"]["date"] = st.date_input("Date")
+            st.session_state["dict_edit_transaction"]["date"] = st.date_input(
+                "Date",
+                value=datetime.datetime.strptime(row['date'],"%d/%m/%Y")
+            )
             st.session_state["dict_edit_transaction"]["type_actif1"] = st.selectbox(
                 "Type actif 1",
-                all_token_df["type"].unique(),
-                index=all_token_df["type"].unique().__index__(),
+                type_actif_list,
+                index=type_actif_list.index(row['type_actif1']),
             )
             st.session_state["dict_edit_transaction"]["token1"] = st.selectbox(
-                "Token 1", all_token_df["symbole"]
+                "Token 1", all_token_df["symbole"],
+                index=symbole_list.index(row['token1']),
             )
+
             st.session_state["dict_edit_transaction"]["amount1"] = st.number_input(
                 "Amount 1", value=row["amount1"]
             )
@@ -94,10 +99,12 @@ for idx, row in transaction_df_page.iterrows():
             if row["type_transaction"] == "swap":
                 st.divider()
                 st.session_state["dict_edit_transaction"]["type_actif2"] = st.selectbox(
-                    "Type actif 2", all_token_df["type"].unique()
+                    "Type actif 2", type_actif_list,
+                    index=type_actif_list.index(row['type_actif2']),
                 )
                 st.session_state["dict_edit_transaction"]["token2"] = st.selectbox(
-                    "Token 2", all_token_df["symbole"]
+                    "Token 2", all_token_df["symbole"],
+                    index=symbole_list.index(row['token2']),
                 )
                 st.session_state["dict_edit_transaction"]["amount2"] = st.number_input(
                     "Amount 2"
