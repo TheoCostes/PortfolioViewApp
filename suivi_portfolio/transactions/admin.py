4c34fed7682c4dbdded01cbb61ef72bb28a59978
TheoCostes <theocostes77@gmail.com>
2023-12-22 15:04:06 +0100

    delete django version

--- suivi_portfolio/transactions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Token, Transaction_history, Type_actif, Type_transac
-# Register your models here.
-
-admin.site.register(Transaction_history)
-admin.site.register(Type_actif)
-admin.site.register(Type_transac)
-admin.site.register(Token)
