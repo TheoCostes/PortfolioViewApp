0dddefe488db2a6bee2e812641608487f34b7406
TheoCostes <78727529+TheoCostes@users.noreply.github.com>
2024-01-15 11:03:42 +0100

    Update collect_data.py

--- code_api_aws/collect_data.py
+++ code_api_aws/collect_data.py
@@ -3,14 +3,15 @@ import boto3
 from io import StringIO
 from utils_api import update_prices
 import os
+import secrets
 
 os.chdir("..")
 
 s3_bucket = "dashboard-invest"
 s3_key = "portefeuille.csv"
 
-AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_ACCESS_KEY_ID = secrets.get_secret("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = secrets.get_secret"AWS_SECRET_ACCESS_KEY")
 
 print(AWS_ACCESS_KEY_ID)
 print(AWS_SECRET_ACCESS_KEY)
